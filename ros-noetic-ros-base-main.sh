6cdb9ad1e7f3949e7a95e746b82f4d9987f2f6b3
Daisuke Sato <tiryoh@gmail.com>
2021-12-03 10:04:12 +0900

    Fix #18

--- ros-noetic-ros-base-main.sh
+++ ros-noetic-ros-base-main.sh
@@ -4,7 +4,7 @@ set -eu
 [[ "$(lsb_release -sc)" == "focal" ]] || exit 1
 ROS_DISTRO=noetic
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros-testing/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-testing.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt-get install -y curl
 curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
